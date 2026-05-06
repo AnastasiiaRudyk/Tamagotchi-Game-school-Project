842b59663dcf306dc11308ec58545fc3a50fdfef
Anastasiia Rudyk <1100134128@edu.cbe.ab.ca>
2026-05-06 09:44:33 -0600

    changes

--- script1.js
+++ script1.js
@@ -15,49 +15,51 @@ function toggleWater1() {
         water1.style.display = "none";
     }
 }
+
 class Pet {
-    constructor() {
-      this.hunger = 50;
-      this.hygiene = 50;
-      this.sleep = 50;
-      this.joy = 50;
-      this.room = 'bathroom';
-    }
-  
-    tick() {
-      this.hunger -= 2;
-      this.hygiene -= 2;
-      this.sleep -= 2;
-      this.joy -= 2;
-  
-      if (this.room === 'bathroom') this.hygiene += 12;
-      if (this.room === 'kitchen') this.hunger += 12;
-      if (this.room === 'bedroom') { 
-        this.sleep += 12; 
-        this.joy += 7; 
-      }
-  
-      this.render();
-    }
-  
-    render() {
-      document.getElementById('num1').innerText = this.hunger;
-      document.getElementById('num2').innerText = this.hygiene;
-      document.getElementById('num3').innerText = this.sleep;
-      document.getElementById('num4').innerText = this.joy;
-    }
+  constructor() {
+    this.hunger = 50;
+    this.hygiene = 50;
+    this.sleep = 50;
+    this.joy = 50;
+    this.currentRoom = "bathroom-wrapper";
   }
-  
-  const myPet = new Pet();
-  setInterval(() => myPet.tick(), 1000);
-  
-  function showRoom(name) {
-    document.querySelectorAll('[class$="-wrapper"]').forEach(el => {
-      el.style.display = 'none';
-    });
-    
-    const active = document.querySelector(`.${name}-wrapper`);
-    if (active) active.style.display = 'flex';
-    myPet.room = name;
+
+  tick() {
+    this.hunger -= 2;
+    this.hygiene -= 2;
+    this.sleep -= 2;
+    this.joy -= 2;
+
+    if (this.currentRoom === 'bathroom-wrapper') this.hygiene += 12;
+    if (this.currentRoom === 'kitchen-wrapper') this.hunger += 12;
+    if (this.currentRoom === 'bedroom-wrapper') {
+      this.sleep += 12;
+      this.joy += 7;
+    }
+
+    this.render();
   }
-  
+
+  render() {
+    document.getElementById('num1').innerText = this.hunger;
+    document.getElementById('num2').innerText = this.hygiene;
+    document.getElementById('num3').innerText = this.sleep;
+    document.getElementById('num4').innerText = this.joy;
+  }
+}
+
+const myPet = new Pet();
+setInterval(() => myPet.tick(), 1000);
+
+function showRoom(name) {
+  document.querySelector('.bathroom-wrapper').style.display = 'none';
+  document.querySelector('.bedroom-wrapper').style.display = 'none';
+  document.querySelector('.kitchen-wrapper').style.display = 'none';
+
+  const active = document.querySelector('.' + name);
+  if (active) {
+    active.style.display = 'flex';
+    myPet.currentRoom = name;
+  }
+}
